d10a7df7d826bbf1fdc6cb23f181047662344046
Evan Dancer <evancdancer@gmail.com>
2020-08-15 16:32:43 -0700

    Update colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -4,7 +4,7 @@ const tintColorDark = '#fff';
 export default {
   light: {
     text: '#000',
-    background: '#fff',
+    background: '#f3f5f5',
     tint: tintColorLight,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorLight,

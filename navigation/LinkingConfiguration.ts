ec0cc40e972a1564895631b7055e383361ffefe0
Evan Dancer <evancdancer@gmail.com>
2020-08-15 16:35:09 -0700

    Update linking configuration

--- navigation/LinkingConfiguration.ts
+++ navigation/LinkingConfiguration.ts
@@ -6,14 +6,14 @@ export default {
     screens: {
       Root: {
         screens: {
-          TabOne: {
+          Home: {
             screens: {
-              TabOneScreen: 'one',
+              HomeScreen: 'home',
             },
           },
-          TabTwo: {
+          Profile: {
             screens: {
-              TabTwoScreen: 'two',
+              ProfileScreen: 'profile',
             },
           },
         },

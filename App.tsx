fe1b48cbc60e5429fd323a5cb7cdc2c14d13613c
Evan Dancer <evancdancer@gmail.com>
2020-08-22 13:07:58 -0700

    Add greeting and dummy login

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+import { AuthProvider } from "./contexts/AuthContext";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -14,10 +15,12 @@ export default function App() {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <AuthProvider>
+        <SafeAreaProvider>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </SafeAreaProvider>
+      </AuthProvider>
     );
   }
 }
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,32 +1,55 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import * as React from "react";
+import { TouchableOpacity, StyleSheet } from "react-native";
 
-import { Text, View } from '../components/Themed';
-import TabBar from '../components/TabBar';
+import { Text, View } from "../components/Themed";
+import TabBar from "../components/TabBar";
+
+import { useAuthContext, loginSuccess, logout } from "../contexts/AuthContext";
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
+      <Greeting />
 
       <TabBar />
     </View>
   );
 }
 
+function Greeting() {
+  const { auth, dispatch } = useAuthContext();
+  if (auth.isLoggedIn)
+    return (
+      <Text>
+        Hello, {auth.name}!
+        <TouchableOpacity onPress={() => dispatch(logout())}>
+          <Text>Logout</Text>
+        </TouchableOpacity>
+      </Text>
+    );
+  return (
+    <Text>
+      You are not logged in
+      <TouchableOpacity onPress={() => dispatch(loginSuccess("Evan"))}>
+        <Text>Log In</Text>
+      </TouchableOpacity>
+    </Text>
+  );
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });

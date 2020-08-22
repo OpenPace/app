cda4e85e07101715acdc3ba76814c4b9f860745a
Evan Dancer <evancdancer@gmail.com>
2020-08-22 14:02:07 -0700

    Add link to log in screen

--- screens/SignUpScreen.tsx
+++ screens/SignUpScreen.tsx
@@ -1,10 +1,18 @@
 import * as React from "react";
+import { TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
 import { Text, View } from "../components/Themed";
 
 export default function SignUpScreen() {
+  const { navigate } = useNavigation();
+
   return (
     <View>
-      <Text>Sign Up</Text>
+      <Text>Already have an account?</Text>
+      <TouchableOpacity onPress={() => navigate("LogIn")}>
+        <Text>Sign in</Text>
+      </TouchableOpacity>
     </View>
   );
 }

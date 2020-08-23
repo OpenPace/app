45a8f67abac9b655cae34967abdd9b1e31a24829
Evan Dancer <evancdancer@gmail.com>
2020-08-22 17:54:14 -0700

    Use authContext on profile page

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,23 +1,35 @@
-import * as React from 'react';
-import { Image, StyleSheet, TouchableOpacity, View } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons';
-import Screen from '../components/Screen';
-import Card from '../components/Card';
-import TabBar from '../components/TabBar';
-import { Text } from '../components/Themed';
+import * as React from "react";
+import { Image, StyleSheet, TouchableOpacity, View } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import Screen from "../components/Screen";
+import Card from "../components/Card";
+import TabBar from "../components/TabBar";
+import { Text } from "../components/Themed";
+import { useAuthContext } from "../contexts/AuthContext";
 
-import BaseStyles from '../utils/BaseStyles';
+import BaseStyles from "../utils/BaseStyles";
 
 export default function ProfileScreen() {
+  const { auth } = useAuthContext();
+
+  if (!auth.user) {
+    return null;
+  }
+
   return (
     <Screen style={[BaseStyles.p4]}>
       <View style={[BaseStyles.row, BaseStyles.pb4]}>
         <View>
-          <Image source={{ uri: 'http://www.fillmurray.com/200/200' }} style={[styles.profileImg, BaseStyles.rounded]} />
+          <Image
+            source={{ uri: "http://www.fillmurray.com/200/200" }}
+            style={[styles.profileImg, BaseStyles.rounded]}
+          />
         </View>
 
-        <View style={{flexGrow: 1, marginLeft: 8}}>
-          <Text style={[styles.name]}>Evan Dancer</Text>
+        <View style={{ flexGrow: 1, marginLeft: 8 }}>
+          <Text style={[styles.name]}>
+            {auth.user.firstName} {auth.user.lastName}
+          </Text>
           <Text style={[BaseStyles.textMuted]}>Traverse City, MI</Text>
         </View>
 
@@ -26,22 +38,22 @@ export default function ProfileScreen() {
 
       <Card style={[BaseStyles.row, BaseStyles.p3]}>
         <View style={[BaseStyles.col]}>
-          <TouchableOpacity onPress={() => alert('Hello, world!')}>
+          <TouchableOpacity onPress={() => alert("Hello, world!")}>
             <Text style={[BaseStyles.text, BaseStyles.textCenter]}>Run</Text>
           </TouchableOpacity>
         </View>
         <View style={[BaseStyles.col]}>
-          <TouchableOpacity onPress={() => alert('Hello, world!')}>
+          <TouchableOpacity onPress={() => alert("Hello, world!")}>
             <Text style={[BaseStyles.text, BaseStyles.textCenter]}>Ride</Text>
           </TouchableOpacity>
         </View>
         <View style={[BaseStyles.col]}>
-          <TouchableOpacity onPress={() => alert('Hello, world!')}>
+          <TouchableOpacity onPress={() => alert("Hello, world!")}>
             <Text style={[BaseStyles.text, BaseStyles.textCenter]}>Swim</Text>
           </TouchableOpacity>
         </View>
         <View style={[BaseStyles.col]}>
-          <TouchableOpacity onPress={() => alert('Hello, world!')}>
+          <TouchableOpacity onPress={() => alert("Hello, world!")}>
             <Text style={[BaseStyles.text, BaseStyles.textCenter]}>Other</Text>
           </TouchableOpacity>
         </View>
@@ -59,6 +71,6 @@ const styles = StyleSheet.create({
   },
   name: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
 });

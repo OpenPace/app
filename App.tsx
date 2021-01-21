cd94b9ef54895bd8deaf6e88544fd51fbc53fa15
Evan Dancer <evancdancer@gmail.com>
2021-01-20 20:48:00 -0800

    feat: add expo-notifications support

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Platform } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
 
 import useCachedResources from "./hooks/useCachedResources";
 import Navigation from "./navigation";
@@ -7,8 +10,30 @@ import { AuthProvider } from "./contexts/AuthContext";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import StatusBar from "./components/StatusBar";
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
 export default function App(): React.ReactNode {
   const isLoadingComplete = useCachedResources();
+
+  useEffect(() => {
+    if (Platform.OS === "web") {
+      return;
+    }
+
+    registerForPushNotificationsAsync().then(token => alert(token));
+
+    // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
+    Notifications.addNotificationResponseReceivedListener(response => {
+      alert(response);
+    });
+  }, []);
+
 
   if (!isLoadingComplete) {
     return null;
@@ -24,4 +49,37 @@ export default function App(): React.ReactNode {
       </ThemeProvider>
     </AuthProvider>
   );
+}
+
+async function registerForPushNotificationsAsync() {
+  let token: string|null = null;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return null;
+    }
+
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
 }

ff31b0c291b04fc66ae5c90f75aa05ced6768d00
Evan Dancer <evancdancer@gmail.com>
2021-01-22 21:42:41 -0800

    fix: overflow dot calendar issue

--- components/DotCalendar.tsx
+++ components/DotCalendar.tsx
@@ -4,7 +4,9 @@ import { Avatar, Surface, Text } from "react-native-paper";
 import { DateTime } from "luxon";
 
 import Activity from "../api/models/Activity";
+import Colors from "../constants/Colors";
 import { useAuthContext } from "../contexts/AuthContext";
+import { useThemeContext } from "../contexts/ThemeContext";
 import { getActivitiesByDates } from "../services/ActivityService";
 import { StringKeyable } from "../utils";
 import BaseStyles from "../utils/BaseStyles";
@@ -25,6 +27,8 @@ function Label({ text }: { text: string }) {
 
 export default function DotCalendar() {
   const { auth } = useAuthContext();
+  const { scheme } = useThemeContext();
+  const colors = Colors[scheme];
   const startDate = DateTime.local().startOf("month").startOf("week");
   const endDate = DateTime.local().endOf("month").endOf("week").startOf("day");
   const today = DateTime.local();
@@ -59,10 +63,12 @@ export default function DotCalendar() {
         <Avatar.Text
           style={[{ backgroundColor: bgColor }, BaseStyles.mb2, BaseStyles.mt2]}
           size={30}
-          label={date.day}
+          label={date.day.toString()}
         />
 
-        {isToday && <View style={styles.todayDot} />}
+        {isToday && (
+          <View style={[styles.todayDot, { backgroundColor: colors.danger }]} />
+        )}
       </View>
     );
   }
@@ -72,19 +78,35 @@ export default function DotCalendar() {
     return <Item key={date.toISODate()} date={date} />;
   });
 
-  const labels = weekLabels.map((label) => <Label key={label} text={label} />);
+  const labels = weekLabels.map((label, idx) => (
+    <Label key={idx} text={label} />
+  ));
 
   return (
     <Surface style={[BaseStyles.p3, BaseStyles.rounded]}>
       <View style={[BaseStyles.row]}>{labels}</View>
 
-      <View style={[BaseStyles.row]}>{items}</View>
+      <View style={{ flexDirection: "row" }}>
+        <View style={[styles.dotRow]}>{items}</View>
+      </View>
     </Surface>
   );
 }
 
 const styles = StyleSheet.create({
-  dotCol: { flex: 1, flexGrow: 1, minWidth: "14%", position: "relative" },
+  dotRow: {
+    flex: 1,
+    flexBasis: "auto",
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  dotCol: {
+    height: 50,
+    flex: 1,
+    flexGrow: 1,
+    minWidth: "14%",
+    position: "relative",
+  },
   dot: {
     width: 50,
     height: 50,
@@ -94,7 +116,6 @@ const styles = StyleSheet.create({
     width: 4,
     height: 4,
     borderRadius: 2,
-    backgroundColor: "red",
     position: "absolute",
     bottom: 0,
   },

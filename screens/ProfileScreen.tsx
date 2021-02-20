b8b1d66f3d4e0697972fe3cc6d45c56140e876f1
Evan Dancer <evancdancer@gmail.com>
2021-02-19 21:31:39 -0800

    fix: refactor dot calendar

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,18 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, View } from "react-native";
-import { Headline, IconButton, Text } from "react-native-paper";
+import { IconButton, Text } from "react-native-paper";
 import { useNavigation } from "@react-navigation/native";
 
 import Screen from "../components/Screen";
-import DotCalendar from "../components/DotCalendar";
 import UserAvatar from "../components/UserAvatar";
 import { useUserContext } from "../contexts/UserContext";
 
+import { StringKeyable } from "../utils";
 import BaseStyles from "../utils/BaseStyles";
+import { Calendar, DateObject } from "react-native-calendars";
+import { useThemeContext } from "../contexts/ThemeContext";
+import Colors from "../constants/Colors";
+import { getActivitiesByDates } from "../services/ActivityService";
+import { DateTime } from "luxon";
+import { useAuthContext } from "../contexts/AuthContext";
+import Activity from "../api/models/Activity";
 
 export default function ProfileScreen() {
   const { navigate } = useNavigation();
+  const { auth } = useAuthContext();
   const { user } = useUserContext();
+  const { scheme } = useThemeContext();
+  const [activities, setActivities] = useState<Activity[]>([]);
+  const colors = Colors[scheme];
+
+  function getActivitiesByMonth(day: DateObject) {
+    const date = DateTime.fromISO(day.dateString);
+    getActivitiesAroundDate(date);
+  }
+
+  function getActivitiesAroundDate(date: DateTime) {
+    const startDate = date.minus({ month: 1 }).startOf("month").startOf("week");
+    const endDate = date
+      .plus({ month: 1 })
+      .endOf("month")
+      .endOf("week")
+      .startOf("day");
+
+    getActivitiesByDates(startDate, endDate, { authToken: auth.token }).then(
+      setActivities,
+    );
+  }
+
+  useEffect(() => {
+    getActivitiesAroundDate(DateTime.local());
+  }, []);
+
+  const markedDates: StringKeyable = {};
+  const markerColor = colors.accent;
+
+  activities.forEach((activity) => {
+    markedDates[activity.startAt.toISODate()] = {
+      customStyles: {
+        container: {
+          backgroundColor: markerColor,
+        },
+      },
+    };
+  });
 
   return (
     <Screen>
@@ -38,9 +84,24 @@ export default function ProfileScreen() {
       </View>
 
       <View style={[BaseStyles.p4]}>
-        <Headline style={[BaseStyles.mb2]}>This Month</Headline>
-
-        <DotCalendar />
+        <Calendar
+          enableSwipeMonths
+          markingType={"custom"}
+          markedDates={markedDates}
+          style={BaseStyles.rounded}
+          firstDay={1}
+          onMonthChange={getActivitiesByMonth}
+          theme={{
+            backgroundColor: colors.surface,
+            calendarBackground: colors.surface,
+            dayTextColor: colors.text,
+            arrowColor: colors.primary,
+            monthTextColor: colors.text,
+            textDayFontSize: 16,
+            textMonthFontSize: 16,
+            textDayHeaderFontSize: 16,
+          }}
+        />
       </View>
     </Screen>
   );

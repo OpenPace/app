0f6a1f20675bfc91bd0e59827819d42114da8e6e
Evan Dancer <evancdancer@gmail.com>
2021-04-02 19:53:58 -0700

    fix: add notification handler

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Bugsnag from "@bugsnag/expo";
+import * as Notifications from "expo-notifications";
 
 import useCachedResources from "./hooks/useCachedResources";
 import Navigation from "./navigation";
@@ -10,13 +11,13 @@ import StatusBar from "./components/StatusBar";
 
 Bugsnag.start();
 
-// Notifications.setNotificationHandler({
-//   handleNotification: async () => ({
-//     shouldShowAlert: true,
-//     shouldPlaySound: false,
-//     shouldSetBadge: false,
-//   }),
-// });
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App(): React.ReactNode {
   const isLoadingComplete = useCachedResources();
